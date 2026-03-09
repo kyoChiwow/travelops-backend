edde770fafe3966c67eb34625b6534c8754dc9da
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-03-09 06:09:09 +0600

    Continue to complete google authentication
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Request, Response } from "express";
+import expressSession from "express-session";
+import passport from "passport";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import { router } from "./app/routes";
-import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(
+  expressSession({ secret: "secret", resave: false, saveUninitialized: false }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -15,10 +15,13 @@ interface EnvConfig {
     SUPER_ADMIN_PASSWORD: string;
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
+    GOOGLE_CALLBACK_URL: string;
+    EXPRESS_SESSION_SECRET: string;
+    FRONTEND_URL: string;
 }
 
 const loadEnvironmentVariables = () : EnvConfig => {
-    const requiredEnvVars: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET"];
+    const requiredEnvVars: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "SUPER_ADMIN_EMAIL", "SUPER_ADMIN_PASSWORD", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRES, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL, EXPRESS_SESSION_SECRET, FRONTEND_URL"];
 
     requiredEnvVars.forEach((key) => {
         if (!process.env[key]) {
@@ -38,6 +41,9 @@ const loadEnvironmentVariables = () : EnvConfig => {
     JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+    EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string
 }
 }
 
