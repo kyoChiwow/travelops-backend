e7b4d3c625a4cb843f5561a3e84f4d4071791061
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-03-09 07:58:51 +0600

    Continue from adding google login check for local strategy
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -9,6 +9,33 @@ import {
 import { Role } from "../modules/user/user.interface";
 import { User } from "../modules/user/user.model";
 import { envVars } from "./env";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcryptjs from "bcryptjs";
+
+passport.use(
+  new LocalStrategy({
+    usernameField: "email",
+    passwordField: "password"
+  }, async (email: string, password: string, done: any) => {
+    try {
+      const isUserExist = await User.findOne({ email });
+      if (!isUserExist) {
+        return done(null, false, { message: "User does not exist!"})
+      }
+
+      const isPasswordMatched = await bcryptjs.compare(password as string, isUserExist.password as string)
+      if (!isPasswordMatched) {
+        return done(null, false, { message: "Password does not match!"})
+      }
+
+      return done(null, isUserExist);
+
+    } catch (error) {
+      console.log(error);
+      return done(error);
+    }
+  })
+)
 
 passport.use(
   new GoogleStrategy(
