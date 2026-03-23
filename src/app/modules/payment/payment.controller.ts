1cb4586596d551f62f8d48294dd171baddd35af7
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-03-23 12:20:30 +0600

    continue to additional api for initializing payment
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -1,20 +1,49 @@
 import { Request, Response } from "express";
+import { envVars } from "../../config/env";
 import { catchAsync } from "../../utils/catchAsync";
+import { PaymentServices } from "./payment.service";
 
 const successPayment = catchAsync(async (req: Request, res: Response) => {
-    
+  const query = req.query;
+  const result = await PaymentServices.successPaymentService(
+    query as Record<string, string>,
+  );
+
+  if (result.success) {
+    res.redirect(
+      `${envVars.SSL.SSL_SUCCESS_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`,
+    );
+  }
 });
 
 const failPayment = catchAsync(async (req: Request, res: Response) => {
-    
+    const query = req.query;
+  const result = await PaymentServices.failPaymentService(
+    query as Record<string, string>,
+  );
+
+  if (!result.success) {
+    res.redirect(
+      `${envVars.SSL.SSL_FAIL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`,
+    );
+  }
 });
 
 const cancelPayment = catchAsync(async (req: Request, res: Response) => {
-    
+    const query = req.query;
+  const result = await PaymentServices.cancelPaymentService(
+    query as Record<string, string>,
+  );
+
+  if (!result.success) {
+    res.redirect(
+      `${envVars.SSL.SSL_CANCEL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`,
+    );
+  }
 });
 
 export const PaymentControllers = {
-    successPayment,
-    failPayment,
-    cancelPayment
-}
+  successPayment,
+  failPayment,
+  cancelPayment,
+};
--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,18 +1,121 @@
+import { BOOKING_STATUS } from "../booking/booking.interface";
+import { Booking } from "../booking/booking.model";
+import { PAYMENT_STATUS } from "./payment.interface";
+import { Payment } from "./payment.model";
 
-const successPaymentService = async () => {
-    return {};
+const successPaymentService = async (query: Record<string, string>) => {
+  /**
+   * Update Payment status to PAID
+   * Update Booking status to CONFIRM
+   * Also use transaction rollback as there are two different collection that needs to be updated
+   */
+
+  const session = await Booking.startSession();
+  session.startTransaction();
+
+  try {
+    const updatedPayment = await Payment.findOneAndUpdate(
+      {
+        transactionId: query.transactionId,
+      },
+      {
+        status: PAYMENT_STATUS.PAID,
+      },
+      { new: true, runValidators: true, session },
+    );
+
+    await Booking.findByIdAndUpdate(
+      updatedPayment?.booking,
+      { status: BOOKING_STATUS.COMPLETE },
+      { runValidators: true, session },
+    )
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return {
+        success: true,
+        message: "Payment completed successfully!"
+    }
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  }
 };
 
-const failPaymentService = async () => {
-    return {};
+const failPaymentService = async (query: Record<string, string>) => {
+  const session = await Booking.startSession();
+  session.startTransaction();
+
+  try {
+    const updatedPayment = await Payment.findOneAndUpdate(
+      {
+        transactionId: query.transactionId,
+      },
+      {
+        status: PAYMENT_STATUS.FAILED,
+      },
+      { new: true, runValidators: true, session },
+    );
+
+    await Booking.findByIdAndUpdate(
+      updatedPayment?.booking,
+      { status: BOOKING_STATUS.FAILED },
+      { runValidators: true, session },
+    )
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return {
+        success: false,
+        message: "Payment Failed!"
+    }
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  }
 };
 
-const cancelPaymentService = async () => {
-    return {};
+const cancelPaymentService = async (query: Record<string, string>) => {
+  const session = await Booking.startSession();
+  session.startTransaction();
+
+  try {
+    const updatedPayment = await Payment.findOneAndUpdate(
+      {
+        transactionId: query.transactionId,
+      },
+      {
+        status: PAYMENT_STATUS.CANCELLED,
+      },
+      { new: true, runValidators: true, session },
+    );
+
+    await Booking.findByIdAndUpdate(
+      updatedPayment?.booking,
+      { status: BOOKING_STATUS.CANCELLED },
+      { runValidators: true, session },
+    )
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return {
+        success: false,
+        message: "Payment Cancelled!"
+    }
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  }
 };
 
 export const PaymentServices = {
-    successPaymentService,
-    failPaymentService,
-    cancelPaymentService
-}
+  successPaymentService,
+  failPaymentService,
+  cancelPaymentService,
+};
