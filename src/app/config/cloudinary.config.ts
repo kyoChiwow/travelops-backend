92200e72c68ceea2face512e214a95ca312862db
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-03-24 11:28:44 +0600

    move onto upload image for division
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

---
+++ src/app/config/cloudinary.config.ts
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from "cloudinary";
+import { envVars } from "./env";
+
+// Frontend --> Form Data with Image file --> Multer --> Form Data --> Req (Body, file)
+
+// Our folder --> Image --> Form Data --> File --> Multer --> Stores inside temp folder --> Req.file
+
+// Req.file --> Cloudinary (req.file) --> Gives us URL --> Mongoose --> MongoDB
+
+// After using multer-storage-cloudinary
+// Our Folder --> Image --> Form Data --> File --> Multer --> Store inside temp folder (inside cloudinary) --> URL --> req.file --> URL --> req.file --> Mongoose --> MongoDB
+
+cloudinary.config({
+    cloud_name: envVars.CLOUDINARY.CLOUDINARY_CLOUD_NAME,
+    api_key: envVars.CLOUDINARY.CLOUDINARY_API_KEY,
+    api_secret: envVars.CLOUDINARY.CLOUDINARY_SECRET_KEY
+})
+
+export const cloudinaryUpload = cloudinary;
+
