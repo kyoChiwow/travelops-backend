1c02e025bff87cd3e7eaeb7e55d16b574b818280
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-04-05 11:32:46 +0600

    made some changes
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 interface EnvConfig {
   PORT: string;
   DB_URL: string;
-  NODE_ENV: string;
+  NODE_ENV: "development" | "production";
   BCRYPT_SALT_ROUND: string;
   JWT_ACCESS_EXPIRES: string;
   JWT_ACCESS_SECRET: string;
@@ -99,7 +99,7 @@ const loadEnvironmentVariables = (): EnvConfig => {
   return {
     PORT: process.env.PORT as string,
     DB_URL: process.env.DB_URL as string,
-    NODE_ENV: process.env.NODE_ENV as string,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
     BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUNDS as string,
     JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
