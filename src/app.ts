9252551b9d248400dcf88c998d358452f3af621d
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-04-19 00:57:44 +0600

    added file limit here
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,8 @@ app.use(
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(
   cors({
     origin: envVars.FRONTEND_URL,
